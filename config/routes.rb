35cad122f9b690c7719048e0726b19345d15d348
Shashi Singh <shashi.we@gmail.com>
2018-02-13 22:07:36 +0530

    Added devise controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions', 
+    registrations: 'users/registrations', 
+    passwords: 'users/passwords',
+    confirmations: 'users/confirmations' 
+  }
+  
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
